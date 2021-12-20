6d05bc9849d312704d4c5b1708b391a41c33d648
Amatov-Nurs <96337255+Amatov-Nurs@users.noreply.github.com>
2021-12-21 01:00:45 +0600

    Add files via upload

---
+++ nursultan_homework_6.py
@@ -0,0 +1,12 @@
+class Sum_num:
+    def __init__(self):
+        self.numbers = [2, 7, 11, 15]
+        self.desired_sum = 9
+
+    def summ(self, numbers, desired_sum):
+        for i in numbers:
+            for a in numbers:
+                if i + a == desired_sum:
+                    print(numbers.index(i),numbers.index(a))
+test = Sum_num()
+test.summ(test.numbers,test.desired_sum)
